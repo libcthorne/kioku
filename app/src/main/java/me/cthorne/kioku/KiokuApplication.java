19b4f85d9c3a346a0ce1ef2e4611fc2ffbc24407
Christopher Thorne <libcthorne@gmail.com>
2025-05-08 16:31:35 +0100

    Fix publish issues

---
+++ app/src/main/java/me/cthorne/kioku/KiokuApplication.java
@@ -0,0 +1,11 @@
+package me.cthorne.kioku;
+
+import androidx.multidex.MultiDexApplication;
+
+public class KiokuApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Initialize app-wide components here if needed
+    }
+} 
